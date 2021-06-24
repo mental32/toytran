cc3a1026d059d933cbd380e84e80b156c4ef43de
mental <m3nta1@yahoo.com>
2021-06-24 22:21:00 +0300

    memoize loop jump targets

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ Tri further expands on BF, here's currently what's added:
 | Operator  | Description                                                                                                                                                                        |
 | --------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
 | ?         | output the integer value at the data pointer (as opposed to writing it directly like `.`)                                                                                          |
+| /         | Enter "no comment" mode where every character is reserved as a potential operator and comments are not allowed to be written                                                       |
+| /         | Exit "no coment" mode (if already enabled.)                                                                                                                                        |
+
+"no comment" mode commands:
+
+| Operator  | Description                                                                                                                                                                        |
+| --------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| $         | increment the data pointer with the current cell (`ptr += tape[ptr]`)                                                                                                              |
--- tri.cxx
+++ tri.cxx
@@ -1,143 +1,232 @@
 // A toy language for funsies.
 
+extern "C"
+{
+#include <termios.h>
+#include <unistd.h>
+}
+
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <map>
 #include <cstdio>
-#include <termios.h>
-#include <unistd.h>
+#include <vector>
+#include <array>
+#include <string_view>
+#include <utility>
+#include <stack>
 
-auto stding_getchar_linux()
+namespace tri
 {
-    // shamelessly stolen from: https://shtrom.ssji.net/skb/getc.html
 
-    struct termios old_tio, new_tio;
-
-    /* get the terminal settings for stdin */
-    tcgetattr(STDIN_FILENO, &old_tio);
-
-    /* we want to keep the old setting to restore them a the end */
-    new_tio = old_tio;
-
-    /* disable canonical mode (buffered i/o) and local echo */
-    new_tio.c_lflag &= (~ICANON & ~ECHO);
-
-    /* set the new settings immediately */
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
-
-    auto c = getchar();
-
-    /* restore the former settings */
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
-
-    return c;
-}
-
-#define TRIE_WORD ssize_t
-void eval(const std::string &input)
-{
-    std::map<TRIE_WORD, TRIE_WORD> tape;
-    TRIE_WORD data_ptr = 0;
-    TRIE_WORD inst_ptr = 0;
-
-    const auto length = input.length();
-
-    do
+    int getchar_unbuffered()
     {
-        auto ch = input[inst_ptr++];
+        // shamelessly stolen from: https://shtrom.ssji.net/skb/getc.html
 
-        switch (ch)
+        struct termios old_tio, new_tio;
+
+        /* get the terminal settings for stdin */
+        tcgetattr(STDIN_FILENO, &old_tio);
+
+        /* we want to keep the old setting to restore them a the end */
+        new_tio = old_tio;
+
+        /* disable canonical mode (buffered i/o) and local echo */
+        new_tio.c_lflag &= (~ICANON & ~ECHO);
+
+        /* set the new settings immediately */
+        tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+
+        auto c = getchar();
+
+        /* restore the former settings */
+        tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+
+        return c;
+    }
+
+    using TriWord = uint16_t;
+    using TriMap = ::std::map<TriWord, TriWord>;
+
+    void log(const TriMap &tape, TriWord inst_ptr, TriWord data_ptr, char ch)
+    {
+        ::std::cerr << "[" << inst_ptr << ":" << data_ptr << "]: " << tape.at(data_ptr) << ", " << ch << ::std::endl;
+    }
+
+    int linear_scan(const ::std::string_view &sub, bool forward, char left, char right)
+    {
+        int skip = 0;
+        int n = 0;
+
+        auto start = forward ? sub.cbegin() : sub.cend();
+        auto end = forward ? sub.cend() : sub.cbegin();
+
+        auto target = forward ? right : left;
+        auto clobber = forward ? left : right;
+
+        for (auto ptr = start; ptr != end; (forward ? ptr++ : ptr--))
         {
-        case '+':
-            tape[data_ptr]++;
-            break;
-        case '-':
-            tape[data_ptr]--;
-            break;
-        case '>':
-            data_ptr++;
-            break;
-        case '<':
-            data_ptr--;
-            break;
-        case '[':
-            if (tape[data_ptr] == 0)
-            {
-                int skip = 0;
+            n++;
 
-                while (inst_ptr <= length)
+            if (*ptr == target && skip-- == 0)
+            {
+                return n;
+            }
+
+            if (*ptr == clobber)
+            {
+                skip++;
+            }
+        }
+
+        return n;
+    }
+
+    void eval(const std::string &input)
+    {
+        TriMap tape;
+        TriMap loops;
+
+        TriWord data_ptr = 0;
+        TriWord inst_ptr = 0;
+
+        const auto length = input.length();
+        bool no_comment = false;
+
+        do
+        {
+            auto ch = input.at(inst_ptr++);
+
+            switch (ch)
+            {
+            case '+':
+                tape[data_ptr]++;
+                break;
+            case '-':
+                tape[data_ptr]--;
+                break;
+            case 'Z':
+                tape[data_ptr] = 0;
+                break;
+            case '>':
+                data_ptr++;
+                break;
+            case '<':
+                data_ptr--;
+                break;
+            case '[':
+                if (tape[data_ptr] == 0)
                 {
-                    if (input[inst_ptr] == ']')
+                    auto search = loops.find(inst_ptr - 1);
+
+                    if (search == loops.cend())
                     {
-                        if (skip == 0)
-                        {
-                            inst_ptr++;
-                            break;
-                        }
-                        else
-                        {
-                            skip--;
-                            inst_ptr++;
-                        }
-                    }
-                    else if (input[inst_ptr] == '[')
-                    {
-                        skip++;
-                        inst_ptr++;
+                        auto delta = linear_scan(input.substr(inst_ptr, input.length()), true, '[', ']');
+                        loops.insert({inst_ptr - 1, inst_ptr + delta});
+                        inst_ptr += delta;
                     }
                     else
                     {
-                        inst_ptr++;
+                        inst_ptr = std::get<1>(*search);
                     }
                 }
-            }
-            break;
-        case ']':
-            if (tape[data_ptr] != 0)
-            {
-                int skip = 0;
 
-                inst_ptr--;
-
-                while (--inst_ptr > 0)
+                break;
+            case ']':
+                if (tape[data_ptr] != 0)
                 {
-                    if (input[inst_ptr] == ']')
+                    auto search = loops.find(inst_ptr - 1);
+
+                    if (search == loops.cend())
                     {
-                        skip++;
+                        auto delta = linear_scan(input.substr(0, inst_ptr - 2), false, '[', ']');
+                        loops.insert({inst_ptr - 1, inst_ptr - delta});
+                        inst_ptr -= delta;
                     }
-                    else if (input[inst_ptr] == '[')
+                    else
                     {
-                        if (skip == 0)
-                        {
-                            inst_ptr++;
-                            break;
-                        }
-                        else
-                        {
-                            skip--;
-                        }
+                        inst_ptr = std::get<1>(*search);
                     }
                 }
+                break;
+            case '?':
+                std::cout << "[" << data_ptr << "]: " << tape[data_ptr] << std::endl;
+                break;
+            case ',':
+                tape[data_ptr] = getchar_unbuffered();
+                break;
+            case '.':
+                std::cout << static_cast<char>(tape[data_ptr]);
+                break;
+            default:
+                if (ch == '/')
+                {
+                    no_comment = !no_comment;
+                    break;
+                }
+
+                if (no_comment)
+                {
+                    switch (ch)
+                    {
+                    case '$':
+                        data_ptr += tape[data_ptr];
+                        break;
+                    default:
+                        std::cout << "unrecognized operator: " << ch << std::endl;
+                        break;
+                    }
+                }
+
+                break;
             }
-            break;
-        case '?':
-            std::cout << "[" << data_ptr << "]: " << tape[data_ptr] << std::endl;
-            break;
-        case ',':
-            tape[data_ptr] = stding_getchar_linux();
-            break;
-        case '.':
-            std::cout << static_cast<char>(tape[data_ptr]);
-            break;
-        default:
-            break;
-        }
-    } while (inst_ptr <= length && inst_ptr >= 0);
+        } while (inst_ptr < length && inst_ptr >= 0);
+    }
 }
 
-int main(void)
+static auto read_file_to_string(std::ifstream stream) -> std::string
+{
+    constexpr auto read_size = std::size_t{4096};
+
+    stream.exceptions(std::ios_base::badbit);
+
+    auto out = std::string{};
+    auto buf = std::string(read_size, '\0');
+
+    while (stream.read(&buf[0], read_size))
+    {
+        out.append(buf, 0, static_cast<size_t>(stream.gcount()));
+    }
+
+    out.append(buf, 0, static_cast<size_t>(stream.gcount()));
+
+    return out;
+}
+
+std::size_t replace_all(std::string &inout, std::string_view what, std::string_view with)
+{
+    std::size_t count{};
+    for (std::string::size_type pos{};
+         inout.npos != (pos = inout.find(what.data(), pos, what.length()));
+         pos += with.length(), ++count)
+    {
+        inout.replace(pos, what.length(), with.data(), with.length());
+    }
+    return count;
+}
+
+int main(int argc, char *argv[])
 {
     std::string input;
+
+    if (argc == 2)
+    {
+        input = read_file_to_string(std::ifstream{argv[1]});
+        std::cerr << replace_all(input, "[-]", "Z") << std::endl;
+        tri::eval(input);
+        return 0;
+    }
 
     for (;;)
     {
@@ -150,7 +239,7 @@ int main(void)
         }
         else
         {
-            eval(input);
+            tri::eval(input);
         }
 
         input.clear();
