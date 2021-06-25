6f6ba47e38518eb6c8a717b7b25784563ec680ce
mental <m3nta1@yahoo.com>
2021-06-25 03:07:17 +0300

    add more opts

--- tri.cxx
+++ tri.cxx
@@ -4,6 +4,7 @@ extern "C"
 {
 #include <termios.h>
 #include <unistd.h>
+#include <cassert>
 }
 
 #include <fstream>
@@ -80,6 +81,7 @@ struct Instr
 {
     char op;
     TriWord arg;
+    TriWord arg2;
 };
 
 std::vector<Instr> parse(const std::string &input)
@@ -98,17 +100,21 @@ std::vector<Instr> reduce_rle(std::vector<Instr> program)
 {
     std::vector<Instr> rle_program;
     rle_program.reserve(program.size());
-    rle_program.push_back(Instr{program.at(0).op, 0});
 
     for (auto &inst : program)
     {
-        Instr &top = rle_program.back();
-
-        if (inst.op == top.op && inst.op != '[' && inst.op != ']')
+        if (rle_program.size() != 0)
         {
-            top.arg += inst.arg;
+            Instr &top = rle_program.back();
+
+            if (inst.op == top.op && inst.op != '[' && inst.op != ']')
+            {
+                top.arg += inst.arg;
+                continue;
+            }
         }
-        else if (inst.op == '[' || inst.op == ']' || inst.op == '+' || inst.op == '-' || inst.op == '<' || inst.op == '>' || inst.op == '.' || inst.op == ',')
+
+        if (inst.op == '[' || inst.op == ']' || inst.op == '+' || inst.op == '-' || inst.op == '<' || inst.op == '>' || inst.op == '.' || inst.op == ',')
         {
             rle_program.push_back(Instr{inst.op, inst.arg});
         }
@@ -117,23 +123,93 @@ std::vector<Instr> reduce_rle(std::vector<Instr> program)
     return rle_program;
 }
 
-void eval(const std::string &input)
+void optimize(std::vector<Instr> &rle_program)
 {
-    std::vector<Instr> raw_program = parse(std::move(input));
-    std::vector<Instr> rle_program = reduce_rle(std::move(raw_program));
 
-    int offset = 0;
+    uint16_t offset = 0;
     for (auto &instr : rle_program)
     {
         switch (instr.op)
         {
         case '[':
-            instr.arg = linear_scan(rle_program.cbegin() + 1 + offset, rle_program.end(), ']', '[', 1);
+            instr.arg = linear_scan(rle_program.cbegin() + (offset + 1), rle_program.end(), ']', '[', 1);
             break;
 
         case ']':
-            instr.arg = linear_scan(rle_program.cbegin() - 1 + offset, rle_program.cbegin(), '[', ']', -1);
+        {
+            instr.arg = linear_scan(rle_program.cbegin() + (offset - 1), rle_program.cbegin(), '[', ']', -1);
+            int start = (offset + 0) - instr.arg;
+
+            // std::cout << "\n";
+
+            // for (auto i = start - 1; i != offset; i++)
+            // {
+            //     std::cout << offset << " " << rle_program[i].op << std::endl;
+            // }
+
+            assert(rle_program[start].op == '[');
+            assert(rle_program[start + rle_program[start].arg].op == ']');
+
+            if (instr.arg == 2 && rle_program[offset - 1].op == '-')
+            {
+                // std::cout << "Detected Clear loop @ " << offset << std::endl;
+                rle_program[offset - 0] = Instr{'Z', 0};
+                rle_program[offset - 1] = Instr{'Z', 0};
+                rle_program[offset - 2] = Instr{'Z', 3};
+            }
+            else if (instr.arg == 2 && rle_program[offset - 1].op == '<')
+            {
+                // std::cout << "Detected Left Scan loop @ " << offset << " " << rle_program[offset - 1].arg << std::endl;
+                TriWord end = offset + 1;
+                rle_program[offset - 2] = Instr{'L', rle_program[offset - 1].arg, end};
+                rle_program[offset - 0] = Instr{'P', 1};
+                rle_program[offset - 1] = Instr{'P', 0};
+            }
+            else if (instr.arg == 2 && rle_program[offset - 1].op == '>')
+            {
+                // std::cout << "Detected Right Scan loop @ " << offset << " " << rle_program[offset - 1].arg << std::endl;
+                TriWord end = offset + 1;
+                rle_program[offset - 2] = Instr{'R', rle_program[offset - 1].arg, end};
+                rle_program[offset - 0] = Instr{'P', 1};
+                rle_program[offset - 1] = Instr{'P', 0};
+            }
+            else if (rle_program[start + 1].op == '-'                            // NOLINT
+                     && rle_program[start + 2].op == '>'                         // NOLINT
+                     && rle_program[start + 3].op == '+'                         // NOLINT
+                     && rle_program[start + 4].op == '<'                         // NOLINT
+                     && rle_program[start + 2].arg == rle_program[start + 4].arg // NOLINT
+                     && rle_program[start + 1].arg == rle_program[start + 3].arg // NOLINT
+                     && rle_program[start + 5].op == ']'                         // NOLINT
+                     && (start + 5) == offset)                                   // NOLINT
+            {
+                // std::cout << "Detected Copy loop @ " << offset << " " << instr.arg << std::endl;
+
+                TriWord cell_delta = rle_program[start + 2].arg;
+                TriWord jump = rle_program[start].arg;
+
+                rle_program[start] = Instr{'C', cell_delta, jump};
+                rle_program[start + 1] = Instr{'[', jump};
+            }
+            else if (rle_program[start + 1].op == '-'                            // NOLINT
+                     && rle_program[start + 2].op == '<'                         // NOLINT
+                     && rle_program[start + 3].op == '+'                         // NOLINT
+                     && rle_program[start + 4].op == '>'                         // NOLINT
+                     && rle_program[start + 2].arg == rle_program[start + 4].arg // NOLINT
+                     && rle_program[start + 1].arg == rle_program[start + 3].arg // NOLINT
+                     && rle_program[start + 5].op == ']'                         // NOLINT
+                     && (start + 5) == offset)                                   // NOLINT
+            {
+                // std::cout << "Detected Copy loop @ " << offset << " " << instr.arg << std::endl;
+
+                TriWord cell_delta = rle_program[start + 2].arg;
+                TriWord jump = rle_program[start].arg;
+
+                rle_program[start] = Instr{'c', cell_delta, jump};
+                rle_program[start + 1] = Instr{'[', jump};
+            }
+
             break;
+        }
 
         default:
             break;
@@ -141,6 +217,14 @@ void eval(const std::string &input)
 
         offset++;
     }
+}
+
+void eval(const std::string &input)
+{
+    std::vector<Instr> raw_program = parse(std::move(input));
+    std::vector<Instr> rle_program = reduce_rle(std::move(raw_program));
+
+    optimize(rle_program);
 
     TriMap tape;
 
@@ -175,12 +259,72 @@ void eval(const std::string &input)
             inst_ptr++;
             break;
 
+        case 'Z':
+            tape[data_ptr] = 0;
+            inst_ptr += instr.arg;
+            break;
+
+        case 'C':
+            // std::cout << "Copy loop @ " << inst_ptr << " " << instr.arg << std::endl;
+            tape[data_ptr + instr.arg] += tape[data_ptr];
+            tape[data_ptr] = 0;
+            inst_ptr++;
+            break;
+
+        case 'c':
+            // std::cout << "Copy loop @ " << inst_ptr << " " << instr.arg << std::endl;
+            tape[data_ptr - instr.arg] += tape[data_ptr];
+            tape[data_ptr] = 0;
+            inst_ptr++;
+            break;
+
+        case 'L':
+            if (tape[data_ptr] == 0)
+            {
+                inst_ptr = instr.arg2;
+                break;
+            }
+
+            while (data_ptr > 0 && tape[data_ptr] != 0)
+            {
+                data_ptr -= instr.arg;
+            }
+
+            inst_ptr = instr.arg2;
+            break;
+
+        case 'R':
+            if (tape[data_ptr] == 0)
+            {
+                inst_ptr = instr.arg2;
+                break;
+            }
+
+            while (data_ptr > 0 && tape[data_ptr] != 0)
+            {
+                data_ptr += instr.arg;
+            }
+
+            inst_ptr = instr.arg2;
+            break;
+
+        case 'P':
+            std::cerr << "Panic! reached UB area! ix=" << inst_ptr << " " << instr.op << ":" << instr.arg << std::endl;
+            return;
+
         case '.':
             for (int i = 0; i < instr.arg; i++)
             {
                 std::cout << static_cast<char>(tape[data_ptr]);
             }
 
+            fflush(stdout);
+
+            inst_ptr++;
+            break;
+
+        case ',':
+            tape[data_ptr] = getchar_unbuffered();
             inst_ptr++;
             break;
 
@@ -213,8 +357,8 @@ void eval(const std::string &input)
         }
     }
 
-    std::cerr << std::endl;
-    std::cerr << inst_ptr;
+    // std::cerr << std::endl;
+    // std::cerr << inst_ptr;
 }
 
 static auto read_file_to_string(std::ifstream stream) -> std::string
